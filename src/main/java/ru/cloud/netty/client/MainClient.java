531066a82859a8dc9cb79091bbcd8f6330ff313d
Polpsi <59441238+Polpsi@users.noreply.github.com>
2020-08-10 13:09:22 +0300

    Lesson4

--- src/main/java/ru/cloud/netty/client/MainClient.java
+++ src/main/java/ru/cloud/netty/client/MainClient.java
@@ -6,19 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainClient extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main.fxml"));
-        Parent root = fxmlLoader.load();
-        primaryStage.setTitle("Box Client");
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+ public class MainClient extends Application {
+     @Override
+     public void start(Stage primaryStage) throws Exception{
+         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main.fxml"));
+         Parent root = fxmlLoader.load();
+         primaryStage.setTitle("Box Client");
+         Scene scene = new Scene(root);
+         primaryStage.setScene(scene);
+         primaryStage.show();
+     }
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         launch(args);
     }
-}
+ }
 
